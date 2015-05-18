ae6eaf6b9d21728fe282bffaa8faa71a3c1f18f7
Colm Britton <colmjude@gmail.com>
2015-05-18 18:05:56 +0100

    add faked expense processing

--- application/being_a_civil_servant/static/js/receipts.js
+++ application/being_a_civil_servant/static/js/receipts.js
@@ -1,23 +1,53 @@
-(function() {
-  var takePic = document.querySelector("#take-picture"),
-      showPic = document.querySelector("#show-picture");
+(function($) {
+  var URL = window.URL || window.webkitURL;
 
-      if( takePic && showPic ) {
-        // change event
-        takePic.onchange = function(ev) {
+  $(function() {
+    var tmpl = $(".receipt-process__container").first();
+    var $desc = $("#item-description");
+    console.log(tmpl);
+    var takePic = document.querySelector("#take-picture");
+
+    var renderExpense = function(imgURL, desc) {
+      var $item = tmpl.clone();
+      $item
+        .removeClass("hidden")
+        .find(".receipt-process__image")
+          .attr("height", 100)
+          .attr("src", imgURL)
+          .on("load", function() {
+            URL.revokeObjectURL(imgURL);
+          })
+        .end()
+        .find(".receipt__description")
+          .text( desc )
+        .end()
+        .appendTo(".receipt__list");
+
+        // process for x seconds then change
+        setTimeout(function(){
+          $item.addClass("receipt--processed");
+        }, 2000);
+    };
+
+    if( takePic ) {
+      // change event
+      takePic.onchange = function(ev) {
+        // check if description is filled in
+        if( $desc.val() !== "" ) {
           // reference to taken pic or chosen file
           var files = ev.target.files,
               file;
           if( files && files.length > 0 ) {
             file = files[0];
-            var URL = window.URL || window.webkitURL;
             var imgURL = URL.createObjectURL(file);
-            showPic.src = imgURL;
-            showPic.height = "100";
-            showPic.onload = function() {
-              URL.revokeObjectURL(imgURL);
-            };
+            renderExpense(imgURL, $desc.val());
           }
-        };
-      }
-})();
+        } else {
+          // ask for desc
+          console.log("no description");
+        }
+      };
+    }
+  });
+
+})(jQuery);
