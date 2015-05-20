e9454722f1f4094598948cb9d2c6e3f0e68a4f21
Juan Matthys Uys <opyate@gmail.com>
2015-05-20 11:30:55 +0100

    remove error handler

--- application/app.py
+++ application/app.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 '''The app module, containing the app factory function.'''
-from flask import Flask, render_template
+from flask import Flask
 
 from application.settings import ProdConfig
 from application.assets import assets
@@ -34,7 +34,6 @@ def create_app(config_object=ProdConfig):
     app.config.from_object(config_object)
     register_extensions(app)
     register_blueprints(app)
-    register_errorhandlers(app)
     return app
 
 
@@ -58,14 +57,4 @@ def register_blueprints(app):
     app.register_blueprint(jobs.views.blueprint)
     app.register_blueprint(professions.views.blueprint)
     app.register_blueprint(signup.views.blueprint)
-    return None
-
-
-def register_errorhandlers(app):
-    def render_error(error):
-        # If a HTTPException, pull the `code` attribute; default to 500
-        error_code = getattr(error, 'code', 500)
-        return render_template("{0}.html".format(error_code)), error_code
-    for errcode in [401, 404, 500]:
-        app.errorhandler(errcode)(render_error)
     return None
