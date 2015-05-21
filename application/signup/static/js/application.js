c4da2337c21502a0dfbdf5c5f208ba0ffce1cfcf
Colm Britton <colmjude@gmail.com>
2015-05-21 11:51:02 +0100

    update app settings area

--- application/signup/static/js/application.js
+++ application/signup/static/js/application.js
@@ -5,6 +5,13 @@
       $(".app-submitted__wrap").slideDown('slow');
       this.preventDefault();
     });
+
+    $(".settings__save").on("click", function(e) {
+      e.preventDefault();
+      e.stopPropagation();
+      $(".settings-msg__success").show().fadeOut(3000);
+      return false;
+    });
   });
 
 }(jQuery));
