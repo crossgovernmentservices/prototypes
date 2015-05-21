8ffe7c2d864f2eab95d071dc9758aeac28ebdd73
Colm Britton <colmjude@gmail.com>
2015-05-21 16:16:48 +0100

    edit personal details

--- application/signup/static/js/application.js
+++ application/signup/static/js/application.js
@@ -13,6 +13,21 @@
       $(".settings-msg__success").show().fadeOut(3000);
       return false;
     });
+
+    $(".data-item__edit").on("click", function() {
+      var $item = $(this).parents(".data-item").find(".data-item__entry");
+      var current_val = $item.text();
+      $item
+        .text("")
+        .append( $( document.createElement("input") ).addClass("form-control data-item__edit__input").val(current_val).on("change", saveChange) );
+    });
+
+    var saveChange = function() {
+      var name = $(this).val();
+      var data = {};
+      data[ $(this).parent().data("key") ] = name;
+      $.post( "/signup/profile", data );
+    };
   });
 
 }(jQuery));
