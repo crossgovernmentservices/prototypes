540f4ea622af276822d242fae7ff5a0f81325abf
Colm Britton <colmjude@gmail.com>
2015-05-21 15:27:45 +0100

    fake associating linkedin account

--- application/signup/static/js/basic.js
+++ application/signup/static/js/basic.js
@@ -1,9 +1,16 @@
 ;(function($) {
   'use strict';
 
+  var swapClasses = function($el, remove, add) {
+    $el
+      .removeClass(remove)
+      .addClass(add);
+  };
+
   $(function() {
     var $cvInput = $("#cv-upload");
     var $cvUploadBtn = $(".cv-upload-btn");
+    var $linkedinBtn = $(".linkedin-upload-btn");
 
     $cvUploadBtn.on("click", function(e) {
       $cvInput.trigger("click");
@@ -16,6 +23,17 @@
         $item.addClass("cv-active");
       }, 2000);
     });
+
+    $linkedinBtn.on("click", function(e) {
+      e.preventDefault();
+      var $linkedinItem = $("[data-item-type='linkedin']");
+      swapClasses($linkedinItem, "linkedin--associated-not", "linkedin--associating");
+      window.setTimeout(function() {
+        swapClasses($linkedinItem, "linkedin--associating", "linkedin--associated");
+        $(".skills__extra__wrap").removeClass("skills__extra--no-linkedin");
+      }, 2000);
+    });
+
   });
 
 }(jQuery));
