b49bbf9bf5ba0b00856da78c66e29d98c7fcf285
Colm Britton <colmjude@gmail.com>
2015-05-25 11:18:00 +0100

    update contract signing procedure

--- application/jobs/static/js/contract.js
+++ application/jobs/static/js/contract.js
@@ -1,11 +1,23 @@
 ;(function($) {
   'use strict';
 
+  var sendData = function(data) {
+    $.ajax({
+      type: 'POST',
+      url: '/profile/',
+      contentType: 'application/json; charset=utf-8',
+      dataType: 'json',
+      data: JSON.stringify(data)
+    });
+  };
+
   // on DOM ready
   $(function() {
+    var $label = $(".switch__checkbox__label");
 
     $(".contract").bind("contractSigned", function() {
       $(this).toggleClass("contract--signed");
+      $label.toggleClass("switch__checkbox__label--on");
     });
 
     $(".switch__checkbox").on("change", function() {
@@ -13,8 +25,10 @@
       // need to be the case
       if( $(this).is(':checked') ) {
         $(".contract").trigger("contractSigned");
+        sendData({ "contract_signed": true });
       } else {
         $(".contract").trigger("contractSigned");
+        sendData({ "contract_signed": false });
       }
     });
   });
